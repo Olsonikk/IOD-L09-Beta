b5b66cbd1e327a0a922f376ea54834f1283036c5
BartlomiejRudowicz <147399938+BartlomiejRudowicz@users.noreply.github.com>
2024-12-18 12:48:47 +0100

    Dodano elementy dokumentacji Javadoc

--- src/main/java/pl/put/poznan/transformer/logic/TransformerFactory.java
+++ src/main/java/pl/put/poznan/transformer/logic/TransformerFactory.java
@@ -1,6 +1,16 @@
 package pl.put.poznan.transformer.logic;
 
+/**
+ * Fabryka transformacji tekstu, która tworzy dynamiczne łańcuchy dekoratorów na podstawie listy transformacji.
+ */
 public class TransformerFactory {
+    /**
+     * Tworzy i zwraca instancję transformera tekstu na podstawie zadanych transformacji.
+     *
+     * @param transforms Tablica nazw transformacji, które mają zostać zastosowane w kolejności.
+     * @return Instancja {@code TextTransformerInterface}, która reprezentuje łańcuch transformacji.
+     * @throws IllegalArgumentException jeśli nazwa transformacji jest nieznana.
+     */
     public static TextTransformerInterface createTransformer(String[] transforms) {
         TextTransformerInterface transformer = new BaseTransformer();
 
@@ -30,6 +40,9 @@ public class TransformerFactory {
                         break;
                     case "latex":
                         transformer = new LatexTransformer(transformer);
+                        break;
+                    case "numbertotext":
+                        transformer = new NumberToTextTransformer(transformer);
                         break;
                     default:
                         throw new IllegalArgumentException("Nieznana transformacja: " + transform);
