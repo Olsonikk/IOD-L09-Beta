14dd1e14bd23115864d6d8d131a922de5b7f647e
Alex <olek.czarnecki@gmail.com>
2024-12-18 12:09:32 +0100

    Bug fixed

--- src/main/java/pl/put/poznan/transformer/rest/FormattedTextTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/FormattedTextTransformerController.java
@@ -1,39 +1,5 @@
 package pl.put.poznan.transformer.rest;
 
-<<<<<<< HEAD
-
-
-import org.slf4j.Logger;
-
-import org.slf4j.LoggerFactory;
-
-import org.springframework.http.HttpStatus;
-
-import org.springframework.http.ResponseEntity;
-
-import org.springframework.web.bind.annotation.*;
-
-import pl.put.poznan.transformer.logic.TextTransformer;
-
-import java.util.Arrays;
-
-import org.springframework.web.bind.annotation.RequestBody;
-
-
-/**
- * Kontroler REST do obsługi zapytań HTTP dla transformacji sformatowanego tekstu.
- */
-@RestController
-
-@RequestMapping("/formated")
-
-@CrossOrigin
-
-public class FormattedTextTransformerController {
-
-
-
-=======
 import java.util.Arrays;
 
 import org.slf4j.Logger;
@@ -54,7 +20,6 @@ import pl.put.poznan.transformer.logic.TransformerFactory;
 @CrossOrigin
 public class FormattedTextTransformerController {
 
->>>>>>> master
     private static final Logger logger = LoggerFactory.getLogger(FormattedTextTransformerController.class);
 
     /**
@@ -64,52 +29,6 @@ public class FormattedTextTransformerController {
      * @return Odpowiedź JSON zawierająca przekształcony tekst lub komunikat błędu.
      */
     @RequestMapping(value = "/transform", method = RequestMethod.POST, produces = "application/json")
-<<<<<<< HEAD
-
-    public ResponseEntity<String> postJson(@RequestBody TransformRequest request) {
-
-
-
-        logger.debug("Input text: " + request.getText());
-
-        logger.debug("Transforms: " + Arrays.toString(request.getTransforms()));
-
-
-
-        try {
-
-            // Tworzenie obiektu TextTransformer
-
-            TextTransformer transformer = new TextTransformer(request.getTransforms());
-
-
-
-            // Zwracanie przetworzonego tekstu w formacie JSON
-
-            String jsonResponse = "{\"transformedText\": \"" + transformer.transform(request.getText()) + "\"}";
-
-            return ResponseEntity.ok(jsonResponse);
-
-
-
-        } catch (IllegalArgumentException e) {
-
-            // Obsługa nieznanej transformacji
-
-            logger.error("Error: " + e.getMessage());
-
-            String errorResponse = "{\"error\": \"" + e.getMessage() + "\"}";
-
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
-
-        }
-
-    }
-
-}
-
-// New class to represent the JSON request body
-=======
     public ResponseEntity<String> postJson(@RequestBody TransformRequest request) {
 
         logger.debug("Input text: " + request.getText());
@@ -131,17 +50,12 @@ public class FormattedTextTransformerController {
     }
 }
 
->>>>>>> master
 /**
  * Klasa reprezentująca żądanie JSON dla transformacji tekstu.
  */
 class TransformRequest {
 
     private String text;
-<<<<<<< HEAD
-
-=======
->>>>>>> master
     private String[] transforms;
 
     /**
@@ -150,69 +64,33 @@ class TransformRequest {
      * @return Tekst wejściowy.
      */
     public String getText() {
-<<<<<<< HEAD
-
-        return text;
-
-    }
-
-
-=======
         return text;
     }
 
->>>>>>> master
     /**
      * Ustawia tekst wejściowy do przetworzenia.
      *
      * @param text Tekst wejściowy.
      */
     public void setText(String text) {
-<<<<<<< HEAD
-
-        this.text = text;
-
-    }
-
-
-=======
         this.text = text;
     }
 
->>>>>>> master
     /**
      * Pobiera listę transformacji do zastosowania.
      *
      * @return Tablica transformacji.
      */
     public String[] getTransforms() {
-<<<<<<< HEAD
-
-        return transforms;
-
-    }
-
-
-=======
         return transforms;
     }
 
->>>>>>> master
     /**
      * Ustawia listę transformacji do zastosowania.
      *
      * @param transforms Tablica transformacji.
      */
     public void setTransforms(String[] transforms) {
-<<<<<<< HEAD
-
-        this.transforms = transforms;
-
-    }
-
-}
-=======
         this.transforms = transforms;
     }
 }
->>>>>>> master
--- src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
@@ -1,20 +1,11 @@
 package pl.put.poznan.transformer.rest;
 
-<<<<<<< HEAD
-=======
 import java.util.Arrays;
 
->>>>>>> master
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.*;
-import pl.put.poznan.transformer.logic.TextTransformer;
-
-import java.util.Arrays;
-=======
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import pl.put.poznan.transformer.logic.TextTransformerInterface;
 import pl.put.poznan.transformer.logic.TransformerFactory;
->>>>>>> master
 
 /**
  * Kontroler REST do obsługi zapytań HTTP dla klasycznych transformacji tekstu.
@@ -35,57 +25,31 @@ import pl.put.poznan.transformer.logic.TransformerFactory;
 public class TextTransformerController {
 
     private static final Logger logger = LoggerFactory.getLogger(TextTransformerController.class);
-<<<<<<< HEAD
-    /**
-     * Obsługuje żądanie GET dla transformacji tekstu.
-     *
-     * @param text        Tekst wejściowy, który ma zostać przetworzony.
-     * @param transforms  Lista transformacji do zastosowania na tekście. Jeśli nie podano, domyślną wartością jest "upper".
-=======
 
     /**
      * Obsługuje żądanie GET dla transformacji tekstu.
      *
      * @param text       Tekst wejściowy, który ma zostać przetworzony.
      * @param transforms Lista transformacji do zastosowania na tekście. Jeśli nie podano, domyślną wartością jest "upper".
->>>>>>> master
      * @return Odpowiedź JSON zawierająca przekształcony tekst lub komunikat błędu.
      */
     @RequestMapping(path = "/{text}", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity<String> get(@PathVariable String text,
-<<<<<<< HEAD
-                              @RequestParam(value="transforms", defaultValue="upper") String[] transforms) {
-=======
                                       @RequestParam(value = "transforms", defaultValue = "upper") String[] transforms) {
->>>>>>> master
 
         logger.debug("Input text: " + text);
         logger.debug("Transforms: " + Arrays.toString(transforms));
 
         try {
-<<<<<<< HEAD
-            // Tworzenie obiektu TextTransformer
-            TextTransformer transformer = new TextTransformer(transforms);
-
-            // Zwracanie przetworzonego tekstu
-            return ResponseEntity.ok(transformer.transform(text));
-
-        } catch (IllegalArgumentException e) {
-            // Obsługa nieznanej transformacji
-=======
             // Tworzenie dynamicznego łańcucha dekoratorów
             TextTransformerInterface transformer = TransformerFactory.createTransformer(transforms);
             String result = transformer.transform(text);
 
             return ResponseEntity.ok(result);
         } catch (IllegalArgumentException e) {
->>>>>>> master
             logger.error("Error: " + e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
 }
-<<<<<<< HEAD
-=======
 
->>>>>>> master
